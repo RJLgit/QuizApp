847d2c9bae6be465f7ceb3059ecf92578c541f51
Rich <r.j.lewindon@hotmail.co.uk>
2019-04-15 11:40:10 +0100

    Fixed bug so text always shown and transaction always works

--- app/src/main/java/com/example/android/myquizapp/ResultActivity.java
+++ app/src/main/java/com/example/android/myquizapp/ResultActivity.java
@@ -153,6 +153,10 @@ public class ResultActivity extends AppCompatActivity {
         } else {
             toDisplay = "Your Score was " + intPercentScore + " percent!" + "\n" + "This is not a high score.";
         }
+        //So all data references are written to during the transaction
+        String lastChecked = "";
+        transaction.update(documentReference, "LastTimeChecked", lastChecked);
+        transaction.update(globalDocumentReference, "LastTimeChecked", lastChecked);
         if (yourHighScore) {
             Map<String, Object> upd = new HashMap<>();
             upd.put(category.toLowerCase(), intPercentScore);
