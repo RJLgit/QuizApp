6f3714d91d16bb6d1d88147ddf88edf2eb647a17
Rich <r.j.lewindon@hotmail.co.uk>
2019-05-06 09:18:13 +0100

    Fixed crash from using LocalDate for notifcations which did not have supported methods on API 21, now just different method on below API 26

--- app/src/main/java/com/example/android/myquizapp/GlobalScoreUpdated.java
+++ app/src/main/java/com/example/android/myquizapp/GlobalScoreUpdated.java
@@ -1,5 +1,6 @@
 package com.example.android.myquizapp;
 
+import android.os.Build;
 import android.util.Log;
 
 import com.google.firebase.Timestamp;
@@ -7,6 +8,7 @@ import com.google.firebase.Timestamp;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class GlobalScoreUpdated {
@@ -133,42 +135,84 @@ public class GlobalScoreUpdated {
     public String getUpdatedSince() {
         String result = "";
         ArrayList<String> cats = new ArrayList<>();
-        LocalDate current = LocalDate.now();
-        LocalDate twoDaysAgo = current.minusDays(2);
-        Date twoDaysAgoDate = Date.from(twoDaysAgo.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Log.d(TAG, "getUpdatedSince: ");
-        if (sport != null && sport.toDate().after(twoDaysAgoDate)) {
-            cats.add("Sport");
-        }
-        if (tv != null && tv.toDate().after(twoDaysAgoDate)) {
-            cats.add("Tv");
-        }
-        if (ultimate != null && ultimate.toDate().after(twoDaysAgoDate)) {
-            cats.add("Ultimate");
-        }
-        if (films != null && films.toDate().after(twoDaysAgoDate)) {
-            cats.add("Films");
-        }
-        if (technology != null && technology.toDate().after(twoDaysAgoDate)) {
-            cats.add("Technology");
-        }
-        if (pictures != null && pictures.toDate().after(twoDaysAgoDate)) {
-            cats.add("Pictures");
-        }
-        if (people != null && people.toDate().after(twoDaysAgoDate)) {
-            cats.add("People");
-        }
-        if (nature != null && nature.toDate().after(twoDaysAgoDate)) {
-            cats.add("Nature");
-        }
-        if (music != null && music.toDate().after(twoDaysAgoDate)) {
-            cats.add("Music");
-        }
-        if (history != null && history.toDate().after(twoDaysAgoDate)) {
-            cats.add("History");
-        }
-        if (geography != null && geography.toDate().after(twoDaysAgoDate)) {
-            cats.add("Geography");
+        if (Build.VERSION.SDK_INT >= 26) {
+            LocalDate current = LocalDate.now();
+            LocalDate twoDaysAgo = current.minusDays(2);
+            Date twoDaysAgoDate = Date.from(twoDaysAgo.atStartOfDay(ZoneId.systemDefault()).toInstant());
+            Log.d(TAG, "getUpdatedSince: ");
+            if (sport != null && sport.toDate().after(twoDaysAgoDate)) {
+                cats.add("Sport");
+            }
+            if (tv != null && tv.toDate().after(twoDaysAgoDate)) {
+                cats.add("Tv");
+            }
+            if (ultimate != null && ultimate.toDate().after(twoDaysAgoDate)) {
+                cats.add("Ultimate");
+            }
+            if (films != null && films.toDate().after(twoDaysAgoDate)) {
+                cats.add("Films");
+            }
+            if (technology != null && technology.toDate().after(twoDaysAgoDate)) {
+                cats.add("Technology");
+            }
+            if (pictures != null && pictures.toDate().after(twoDaysAgoDate)) {
+                cats.add("Pictures");
+            }
+            if (people != null && people.toDate().after(twoDaysAgoDate)) {
+                cats.add("People");
+            }
+            if (nature != null && nature.toDate().after(twoDaysAgoDate)) {
+                cats.add("Nature");
+            }
+            if (music != null && music.toDate().after(twoDaysAgoDate)) {
+                cats.add("Music");
+            }
+            if (history != null && history.toDate().after(twoDaysAgoDate)) {
+                cats.add("History");
+            }
+            if (geography != null && geography.toDate().after(twoDaysAgoDate)) {
+                cats.add("Geography");
+            }
+        } else {
+            Date today = new Date();
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(today);
+            cal.add(Calendar.DATE, -2);
+            Date twoDaysAgoDate = cal.getTime();
+            Log.d(TAG, "getUpdatedSince: " + twoDaysAgoDate);
+            if (sport != null && sport.toDate().after(twoDaysAgoDate)) {
+                cats.add("Sport");
+            }
+            if (tv != null && tv.toDate().after(twoDaysAgoDate)) {
+                cats.add("Tv");
+            }
+            if (ultimate != null && ultimate.toDate().after(twoDaysAgoDate)) {
+                cats.add("Ultimate");
+            }
+            if (films != null && films.toDate().after(twoDaysAgoDate)) {
+                cats.add("Films");
+            }
+            if (technology != null && technology.toDate().after(twoDaysAgoDate)) {
+                cats.add("Technology");
+            }
+            if (pictures != null && pictures.toDate().after(twoDaysAgoDate)) {
+                cats.add("Pictures");
+            }
+            if (people != null && people.toDate().after(twoDaysAgoDate)) {
+                cats.add("People");
+            }
+            if (nature != null && nature.toDate().after(twoDaysAgoDate)) {
+                cats.add("Nature");
+            }
+            if (music != null && music.toDate().after(twoDaysAgoDate)) {
+                cats.add("Music");
+            }
+            if (history != null && history.toDate().after(twoDaysAgoDate)) {
+                cats.add("History");
+            }
+            if (geography != null && geography.toDate().after(twoDaysAgoDate)) {
+                cats.add("Geography");
+            }
         }
         if (cats.size() == 0) {
             result = "No top scores have been set in the last two days. Can you set one?";
