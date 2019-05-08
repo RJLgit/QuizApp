d02cb629c341fde30d78235c57310b93125cc5bf
Rich <r.j.lewindon@hotmail.co.uk>
2019-05-08 11:03:01 +0100

    Background color in results recycler view now reflects correct results

--- app/src/main/java/com/example/android/myquizapp/ResultsAdapter.java
+++ app/src/main/java/com/example/android/myquizapp/ResultsAdapter.java
@@ -2,10 +2,12 @@ package com.example.android.myquizapp;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -56,6 +58,8 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsV
         private TextView mUserAnswersTextView;
         private ProgressBar mProgressBar;
         private TextView mCorrectAnswersTextView;
+        private LinearLayout mBackLayout;
+        private ConstraintLayout mBackLayoutCon;
 
         public ResultsViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -63,13 +67,20 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsV
             mUserAnswersTextView = itemView.findViewById(R.id.userAnswerText);
             mCorrectAnswersTextView = itemView.findViewById(R.id.correctAnswerText);
             mProgressBar = itemView.findViewById(R.id.progressBarResults);
-
+            mBackLayout = itemView.findViewById(R.id.backgroundForResultsItems);
+            mBackLayoutCon = itemView.findViewById(R.id.backgroundForResultsItemsConst);
         }
 
         void bind(String quest, String userAns, String corrAns) {
             mQuestionsTextView.setText(quest);
             mUserAnswersTextView.setText(userAns);
             mCorrectAnswersTextView.setText(corrAns);
+            if (userAns.equals(corrAns)) {
+
+                mBackLayoutCon.setBackgroundColor(mContext.getResources().getColor(android.R.color.holo_green_light));
+            } else {
+                mBackLayoutCon.setBackgroundColor(mContext.getResources().getColor(android.R.color.holo_red_light));
+            }
             mQuestionsTextView.setVisibility(View.VISIBLE);
             mUserAnswersTextView.setVisibility(View.VISIBLE);
             mCorrectAnswersTextView.setVisibility(View.VISIBLE);
