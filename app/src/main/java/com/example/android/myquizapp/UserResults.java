e405f5aa71a4e7c7521bd181ac84203df7aadb4e
Rich <r.j.lewindon@hotmail.co.uk>
2019-05-08 10:05:29 +0100

    Added new singleton class to store user results

---
+++ app/src/main/java/com/example/android/myquizapp/UserResults.java
@@ -0,0 +1,24 @@
+package com.example.android.myquizapp;
+
+
+import java.util.ArrayList;
+
+public class UserResults {
+    private static UserResults single_user_results;
+    public ArrayList<String> questions;
+    public ArrayList<String> userAnswers;
+    public ArrayList<String> correctAnswers;
+
+    private UserResults() {
+        questions = new ArrayList<>();
+        userAnswers = new ArrayList<>();
+        correctAnswers = new ArrayList<>();
+    }
+    public static UserResults getInstance() {
+        if (single_user_results == null) {
+            single_user_results = new UserResults();
+        }
+        return single_user_results;
+    }
+
+}
